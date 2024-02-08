20cf708d0e7af22d9249e1d36e5b78bf2c74d827
OyewoleOyedeji <57059138+oyedejioyewole@users.noreply.github.com>
2024-02-08 12:27:30 +0100

    misc(cli): 🖋 Updated package.json

--- cli/index.ts
+++ cli/index.ts
@@ -3,7 +3,7 @@ import runHealthCheck from "./utils/health.js";
 import manage from "./utils/manage/index.js";
 
 (async () => {
-  consola.start("Starting database manager");
+  consola.start("Starting database");
 
   consola.info("Running health check ...");
   await runHealthCheck();
